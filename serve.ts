3ffc27fe10d927709cfe13ab2b6e85202f4fa720
Séverin DUPOUY <severin.dupouy@stormshield.eu>
2023-08-29 15:04:39 +0200

    cache busting assets

--- serve.ts
+++ serve.ts
@@ -1,3 +1,5 @@
+import cache_busting from "lume/middlewares/cache_busting.ts";
+import expires from "lume/middlewares/expires.ts";
 import Server from "lume/core/server.ts";
 
 const port = 8000;
@@ -6,6 +8,8 @@ const server = new Server({
   port,
   root: `${Deno.cwd()}/_site`,
 });
+
+server.use(expires()).use(cache_busting());
 
 server.start();
 
